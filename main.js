337980303828f33ff58eb42b7a550b9679c03185
Om Gehlot <om.gehlot@impetus.co.in>
2018-10-16 12:07:46 +0530

    added week logic

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 window.onload = startup;
-var divId = '#__content9-footer-text';
-
+var div1Id = '#__content9-footer-text';
+var div2Id = '#__content11-footer-text';
 
 
 function fetchDetails(response){
   var punches = response.querySelectorAll("content properties");
   var lastIn = punches[punches.length -1];
   var workingHours = lastIn.querySelector('number').innerHTML;
+  var totalWorkingHours = "00:00:00";
+
+  var allSwipes = {};
+
+  punches.forEach((swipe, index) => {
+    if(!allSwipes[swipe.querySelector('Swipedate').innerHTML]){
+      allSwipes[swipe.querySelector('Swipedate').innerHTML] = swipe.querySelector('number').innerHTML;
+      totalWorkingHours = addWorkingHours(totalWorkingHours, swipe.querySelector('number').innerHTML);
+    }
+  });
+
+  console.log("totalWorkingHours : "+totalWorkingHours);
+
   var punchType = Number.parseInt(lastIn.querySelector('Channel').innerHTML);
   var punchTime = new Date(lastIn.querySelector('Punchdate').innerHTML)
 
   if(punchType===1){
-      var recur = setInterval(function(){
+      setInterval(function(){
         try{
-          let time = calculateTime(punchTime,workingHours);
-          let timeString = time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
-          var element = $(divId).text("current-time:   "+timeString);
+          showTimesOnPage(punchTime, workingHours, totalWorkingHours);
         }catch(err){
           console.error(err)
         }
@@ -23,12 +34,23 @@ function fetchDetails(response){
 
   }else{
     console.log(workingHours)
-    var element = $(divId).text("current-time:   "+timeString);
+    showTimesOnPage(punchTime, "00:00:00", totalWorkingHours);
   }
-
-
 }
+
+function showTimesOnPage(punchTime, workingHours, totalWorkingHours){
+    //Show Todays Time
+    let time = calculateTime(punchTime,workingHours);
+    var todaysTime = time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
+
+    //Show Week Time
+    time = calculateTime(punchTime,totalWorkingHours); 
+    $(div1Id).html("Todays Time : "+todaysTime);   
+    $(div2Id).html("Weeks Time : "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
+}
+
 function calculateTime(punchTime,workingHours){
+  //console.log("workingHours : "+workingHours);
   var currentTime = new Date();
   var timeDifference = (currentTime - punchTime);
   var currentHours = new Date('1970-01-01 '+workingHours);
@@ -36,8 +58,30 @@ function calculateTime(punchTime,workingHours){
   return currentHours;
 }
 
-function getTodaysDate(){
-  var currentDate = new Date();
+function addWorkingHours(workingHour1,workingHour2){
+  let workingHour1Arr = workingHour1.split(':');
+  let workingHour2Arr = workingHour2.split(':');
+  let resultWorkingHourArr= [];
+  let carry = 0;
+
+  for (let i = 2; i >= 0; i--) {
+    let t = +workingHour1Arr[i]+ +workingHour2Arr[i];
+    if(carry > 0)
+        t += carry;
+
+    if(t>= 60 && i > 0){
+      resultWorkingHourArr[i] = t%60;
+      carry = Math.floor(t/60);
+    }else{
+      resultWorkingHourArr[i] = t;
+      carry = 0;
+    }
+  }
+  return resultWorkingHourArr.join(":");
+}
+
+function getTodaysDate(startDate){
+  var currentDate = startDate;
   var stringDate = '';
   stringDate+= currentDate.getFullYear();
   var month = currentDate.getMonth() +1;
@@ -49,8 +93,26 @@ function getTodaysDate(){
 
 function startup(){
 
-  var startDate = getTodaysDate();
-  var endDate = startDate;
+  var todaysDate = new Date();
+  todaysDate.setHours(24,0,0,0)
+  //todaysDate.setDate(todaysDate.getDate() - 3);
+  console.log("todaysDate : "+todaysDate);
+  
+  var endDate = getTodaysDate(todaysDate);
+  console.log("endDate : "+endDate);
+
+  var date = todaysDate;
+  var day = date.getDay();
+  var prevMonday = date;
+  if(date.getDay() == 0){
+    prevMonday.setDate(date.getDate() - 7);
+  }
+  else{
+    prevMonday.setDate(date.getDate() - day);
+  }
+  console.log("prevMonday : "+prevMonday);
+  var startDate = getTodaysDate(prevMonday);  
+
   var employee = {id:null};
   var startUrl = "https://empower.impetus.co.in//sap/bc/ui2/start_up";
 
